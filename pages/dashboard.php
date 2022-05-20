30155066e388748bd0fb8352792a92ce60bc48e3
santribugstreet <nurihefez@gmail.com>
2022-05-20 14:38:17 +0700

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,7 +1,9 @@
+<?php if(!defined("ABSPATH")) exit (); ?>
 <div class="main-content">
      <div class="row">
           <div class="col l12">
                <h5>Dashboard</h5>
+               <!-- akan ditampilkan informasi-informasi seputar pendaftaran -->
 
           </div>
      </div>

bb481b4c565a83442878aa7c007072ae419bfa28
santribugstreet <nurihefez@gmail.com>
2022-05-20 14:42:17 +0700

    Update daftar.php

--- pages/daftar.php
+++ pages/daftar.php
@@ -1,16 +1,26 @@
 <?php 
+    if (!defined("ABSPATH")) exit ();
+    // pesan kosong untuk ditampilkan
+    $pesan = null;
     if (isset($_POST["register"])){
         $pengguna->param=$_POST;
         $respon=$pengguna->pendaftaran();
         if ($respon == "registered") {
             header("location:http://localhost/ppdbmtsnt/?page=login");
         }
+        /**
+         * @respon perlu respon tambahan, seperti jika pendaftaran gagal 
+         * karena sudah terdaftar atau kata sandi yang tidak sama
+         * lanjutan di 'class/pengguna.php'
+         **/
     }
+    
 
 ?>
 
 <div class="container">
     <h5 class="mt-20">Registrasi</h5>
+    <p><?php echo $pesan; /*jika terdapat pesan akan ditampilkan disini*/ ?></p>
     <form action="" class="mb-20" method="POST">
     <div class="input-field">
         <input type="text" placeholder="Nama anda" name="nama">
